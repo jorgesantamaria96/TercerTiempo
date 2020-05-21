f1d3fb9e5dbb00436663ed155383934eef84ebb3
Jorge <jorge.santamaria966@gmail.com>
2020-05-21 16:58:47 -0300

    Colores

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,10 +6,27 @@ export const mobileTT = 'http://';
 const { width, height } = Dimensions.get('window');
 
 export const colors = {
-    ttblue: '#D70A18',
+    ttblue: '#001667',
     ttred: '#D70A18',
+    phoneBorderColor: '#3F3D56',
+    colorPhysicalButton: '#6C63FF',
+    labelIsNotFocused: '#9E9E9E',
+    nextSup: '#BE002A',
+    nextInf: '#FF6868',
+    buttonLoginColor1: '#FF5B7F',
+    buttonLoginColor2: '#FC9272',
+    buttonLoginColor3: '#FC9970',
     background: '#E3E3E3',
-
+    textColor: '#515C6F',
+    termCond: '#FE627D',
+    buttonComenzar: '#F3555D',
+    inicioText: '#656565',
+    ranking: '#FFD500',
+    nameCardAndclockText: '#494A4B',
+    playVideo: '#F3555D',
+    nameAuthorTextColor: '#A5A5A5',
+    tabIconColor: '#8890A6',
+    textInputLabelColor: '#FD7E77',
 }
 
 export { width, height };
