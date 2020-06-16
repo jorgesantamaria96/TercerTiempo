e5df33377f9c9ae137ff2aac68bc23cb1f390e21
Jorge <jorge.santamaria966@gmail.com>
2020-06-16 17:13:59 -0300

    Agregado de imagenes y de AboutUs, resta agregar las imágenes

--- src/screens/home/aboutus/AboutUs.jsx
+++ src/screens/home/aboutus/AboutUs.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
-import { View, StyleSheet, BackHandler, Alert } from "react-native";
+import { View, StyleSheet, BackHandler, ScrollView, Text } from "react-native";
+import { ABOUT } from "../../../data/fundacion";
+import { colors } from "../../../constants/constants";
 
 const AboutUs = ({ route, navigation }) => {
   useEffect(() => {
@@ -16,9 +18,60 @@ const AboutUs = ({ route, navigation }) => {
     return () => backHandler.remove();
   }, []);
 
-  return <View />;
+  const { dni, nombre, apellido, telefono, mail } = route.params;
+  const params = {
+    dni: dni,
+    nombre: nombre,
+    apellido: apellido,
+    telefono: telefono,
+    data: ABOUT,
+    mail: mail,
+  };
+
+  return (
+    <ScrollView>
+      <View style={styles.screen}>
+        <Text style={styles.title}>¡Conocenos un poco!</Text>
+
+        {/* TEXTO */}
+        <View style={styles.textContainer}>
+          {params.data[0].text.map((item, index) => {
+            return (
+              <View style={styles.text} key={index}>
+                <Text style={styles.infoText}>{item.t}</Text>
+              </View>
+            );
+          })}
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    marginTop: 20,
+    paddingHorizontal: 20,
+  },
+  title: {
+    alignItems: "center",
+    textAlign: "center",
+    padding: 10,
+    fontSize: 24,
+    fontWeight: "bold",
+    color: colors.ttblue,
+  },
+  textContainer: {
+    paddingVertical: 20,
+  },
+  text: {
+    paddingVertical: 5,
+  },
+  infoText: {
+      color: colors.inicioText
+  },
+});
 
 export default AboutUs;
